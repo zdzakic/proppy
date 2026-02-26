cecb1ec220a89a4d17f4a1081e6f544dbfbbdd31
Zeljko Dzakic <zdzakic@gmail.com>
2026-02-26 07:28:09 +0100

    basic favico.ico done

--- property-landing/app/layout.tsx
+++ property-landing/app/layout.tsx
@@ -5,6 +5,14 @@ import  Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 
 
+export const metadata = {
+  title: "Rookerys",
+  description: "Property Management Software",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
 export default function RootLayout({
   children,
 }: {
