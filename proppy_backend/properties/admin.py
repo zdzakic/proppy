64237a0c43ce2041935dde6b9c9c79973727d983
Zeljko Dzakic <zdzakic@gmail.com>
2026-03-25 06:46:18 +0100

    feat(admin): improve admin UX and fix autocomplete configuration
    - added UserRookeryRole admin with filters, search and autocomplete
    - added search_fields to CompanyAdmin and PropertyOwnerAdmin (required for autocomplete)
    - improved Property, Block and PropertyOwner admin with search and filters
    - enabled autocomplete_fields for better performance with large datasets
    
    Result:
    - admin is now usable for managing roles and ownership
    - autocomplete works correctly without system check errors
    - improved data navigation and debugging in admin panel

--- proppy_backend/properties/admin.py
+++ proppy_backend/properties/admin.py
@@ -1,22 +1,38 @@
 from django.contrib import admin
-from .models import Company, Block, Property, PropertyOwner
+from .models import Company, Block, Property, PropertyOwner, UserRookeryRole
 
 
 @admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
     list_display = ("name", "is_valid")
+    search_fields = ("name",)
 
 
 @admin.register(Block)
 class BlockAdmin(admin.ModelAdmin):
     list_display = ("name", "company")
+    search_fields = ("name", "company__name")
+    list_filter = ("company",)
 
 
 @admin.register(Property)
 class PropertyAdmin(admin.ModelAdmin):
-    list_display = ("name", "block")  
+    list_display = ("name", "block")
+    search_fields = ("name", "block__name", "block__company__name")
+    list_filter = ("block__company",)
 
 
 @admin.register(PropertyOwner)
 class PropertyOwnerAdmin(admin.ModelAdmin):
     list_display = ("user", "property", "date_from", "date_to")
+    search_fields = ("user__email", "property__name")
+    list_filter = ("property__block__company",)
+    autocomplete_fields = ("user", "property")
+
+
+@admin.register(UserRookeryRole)
+class UserRookeryRoleAdmin(admin.ModelAdmin):
+    list_display = ("user", "company", "role", "property_owner")
+    list_filter = ("role", "company")
+    search_fields = ("user__email", "company__name")
+    autocomplete_fields = ("user", "company", "property_owner")
--- proppy_backend/users/admin.py
+++ proppy_backend/users/admin.py
@@ -17,12 +17,15 @@ class UserAdmin(BaseUserAdmin):
 
     list_display = ("email", "first_name", "last_name", "is_staff")
     ordering = ("email",)
+    list_filter = ("is_active", "is_staff", "is_superuser")
+    readonly_fields = ("date_created",)
+    search_fields = ("email", "first_name", "last_name")
 
     fieldsets = (
         (None, {"fields": ("email", "password")}),
         ("Personal Info", {"fields": ("first_name", "last_name", "title")}),
         ("Address", {"fields": ("address_1", "address_2", "postcode", "country")}),
-        ("Other", {"fields": ("phone", "gender", "date_of_birth", "comment")}),
+        ("Other", {"fields": ("phone", "gender", "date_of_birth", "comment","date_created")}),
         ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
     )
 
@@ -34,35 +37,3 @@ class UserAdmin(BaseUserAdmin):
     )
 
     search_fields = ("email",)
-
-# from django.contrib import admin
-# from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-# from .models import User
-
-# @admin.register(User)
-# class UserAdmin(BaseUserAdmin):
-#     model = User
-#     list_display = ('email', 'first_name', 'last_name', 'phone', 'is_active', 'is_staff')
-#     ordering = ('id',)
-#     list_filter = ('is_staff', 'is_active')
-
-#     fieldsets = (
-#         (None, {
-#             'fields': ('email', 'password', 'first_name', 'last_name', 'address', 'phone', 'date_left', 'comment')
-#         }),
-#         ('Permissions', {
-#             'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
-#         }),
-#     )
-
-#     add_fieldsets = (
-#         (None, {
-#             'classes': ('wide',),
-#             'fields': (
-#                 'email', 'first_name', 'last_name', 'address', 'phone', 'date_left', 'comment',
-#                 'password1', 'password2', 'is_active', 'is_staff'
-#             )}
-#         ),
-#     )
-
-#     search_fields = ('email', 'first_name', 'last_name', 'phone')
